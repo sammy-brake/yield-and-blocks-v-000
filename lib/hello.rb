329c09553ffc171f21db9bbf458a775e2cb032be
Samantha brake <sammy.brake@gmail.com>
2018-08-29 18:55:10 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+counter = 0 
 
+while counter < array.length 
+yield array[counter]
+counter = counter.next 
+end 
+array 
+
+else
+  puts "Hey! No block was given!"
 end
+end 
 
 # call your method here!
 
